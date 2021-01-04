13b621e9b95d45f90736e757d1e64dd375ee85db
logesh <logeshbiochem06@gmail.com>
2021-01-04 12:08:02 +0530

    first commit

---
+++ build/precache-manifest.276f9b61346690ceb3d4d0ada9f6ffe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42cb9ff291a11e5508be1641db11359",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e095d112710c929529c",
+    "url": "./static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "3e095d112710c929529c",
+    "url": "./static/js/2.a0f43ad7.chunk.js"
+  },
+  {
+    "revision": "b85e83f40e13cf19132b069b84ede7b1",
+    "url": "./static/js/2.a0f43ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4629d6f248fe2828a357",
+    "url": "./static/js/main.502d105c.chunk.js"
+  },
+  {
+    "revision": "02c538ed744375ac3d4e",
+    "url": "./static/js/runtime-main.4593c922.js"
+  },
+  {
+    "revision": "613c8a5003b4cdab33be27e2ff8e4b2b",
+    "url": "./static/media/login.613c8a50.jpg"
+  }
+]);
